07c3a88b361d08c58785598ba94b03d7b0c45cab
rafsrootsmh <124554329+Rafiqcompton@users.noreply.github.com>
2023-06-06 23:37:39 +0300

    evaluation_metrics.py
    def compute_metrics(explanations):
        # Compute coverage, fidelity, and comprehensibility metrics
        # ...
    
        metrics = {
            "coverage": coverage,
            "fidelity": fidelity,
            "comprehensibility": comprehensibility
        }
    
        return metrics

---
+++ evaluation_metrics.py
@@ -0,0 +1,11 @@
+def compute_metrics(explanations):
+    # Compute coverage, fidelity, and comprehensibility metrics
+    # ...
+
+    metrics = {
+        "coverage": coverage,
+        "fidelity": fidelity,
+        "comprehensibility": comprehensibility
+    }
+
+    return metrics
